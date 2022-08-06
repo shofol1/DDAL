6ad16cd26faf52ba683b240e8c4fdbabc7b7bfc0
shofol <18103156@iubat.edu>
2022-08-06 18:47:05 +0600

    dot vs bracket notation

---
+++ dotBracketObject.js
@@ -0,0 +1,15 @@
+const person = {
+  firstName: "Ashikul",
+  lastName: "shofol",
+  age: "male",
+};
+
+console.log(person.lastName); //dot notation
+console.log(person["lastName"]); //bracket notation
+
+//dot vs bracket
+
+const nameKey = "Name";
+console.log(person["first" + nameKey]);
+
+//this not work for dot notation
