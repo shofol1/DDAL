87a05fd7a8c614dbcc8d7dbe7ac96028f02ec1eb
shofol <18103156@iubat.edu>
2022-08-07 17:15:07 +0600

    optimize code

--- project_1/script.js
+++ project_1/script.js
@@ -87,12 +87,21 @@ const modalOpenBtn = document.querySelectorAll(".btn-modal");
 const modalCloseBtn = document.querySelector(".close-modal");
 const modal = document.querySelector(".moddal");
 const overlay = document.querySelector(".overlay-hidden ");
+
+for (let i = 0; i < modalOpenBtn.length; i++) {
+  modalOpenBtn[i].addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
 const closeModal = () => {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
+modalCloseBtn.addEventListener("click", closeModal);
 
-for (let i = 0; i < modalOpenBtn.length; i++) {
-  modalOpenBtn[i].addEventListener("click", closeModal());
-}
-modalCloseBtn.addEventListener("click", closeModal());
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
